b18b075e85fb3866d4610a4c8f8c8aff9ec12ab1
user <user@DESKTOP-HA29IMR>
2023-02-01 15:29:31 -0500

    Add project files.

---
+++ Infosys.DBFirstCore.ConsoleUI/Program.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using Infosys.DBFirstCore.DataAccessLayer;
+using Infosys.DBFirstCore.DataAccessLayer.Models;
+
+namespace Infosys.DBFirstCore.ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            QuickKartRepository repository = new QuickKartRepository();
+            var categories = repository.GetAllCategories();
+            Console.WriteLine("------------------------------");
+            Console.WriteLine("CategoryId\tCategoryName");
+            Console.WriteLine("------------------------------");
+
+            // Display all of the Category ID and Category name 
+            foreach(var category in categories)
+            {
+                Console.WriteLine("{0}\t\t{1}", category.CategoryId, category.CategoryName);
+            }
+
+            // Display product by category id
+            Console.WriteLine("\n-----------------------------");
+            Console.WriteLine("Show Product by Category Id");
+            Console.WriteLine("-----------------------------");
+            byte categoryId = 1;
+            List<Product> products = repository.GetProductsOnCategoryId(categoryId);
+
+            if(products.Count == 0)
+            {
+                Console.WriteLine("No Products available under the category: " + categoryId);
+            }
+            else
+            {
+                Console.WriteLine("{0,-15}{1,-30}{2,-15}{3,-10}{4}", "ProductId","ProductName","CategoryId","Price","QuantityAvailable");
+                Console.WriteLine("---------------------------------------------------------------------------------------");
+                foreach(var product in products)
+                {
+                    Console.WriteLine("{0,-15}{1,-30}{2,-15}{3,-10}{4}", product.ProductId, product.ProductName, product.CategoryId,product.Price,product.QuantityAvailable);
+                }
+            }
+
+            // Display product by category id
+            Console.WriteLine("\n-----------------------------");
+            Console.WriteLine("Filter To The End of Product");
+            Console.WriteLine("-----------------------------");
+            Product prod = repository.FilterProducts(categoryId);
+
+            if (products.Count == 0)
+            {
+                Console.WriteLine("No Products available under the category: " + categoryId);
+            }
+            else
+            {
+                Console.WriteLine("{0,-15}{1,-30}{2,-15}{3,-10}{4}", "ProductId", "ProductName", "CategoryId", "Price", "QuantityAvailable");
+                Console.WriteLine("---------------------------------------------------------------------------------------");
+                Console.WriteLine("{0,-15}{1,-30}{2,-15}{3,-10}{4}", prod.ProductId, prod.ProductName, prod.CategoryId, prod.Price, prod.QuantityAvailable);
+            }
+
+            // Display product by pattern
+            Console.WriteLine("\n-----------------------------");
+            Console.WriteLine("Filter Product by Pattern");
+            Console.WriteLine("-----------------------------");
+            string pat = "BMW%";
+            List<Product> lstProd = repository.FilterProductsUsingLikes(pat);
+            if (lstProd.Count == 0)
+            {
+                Console.WriteLine("No Products available under the category: " + categoryId);
+            }
+            else
+            {
+                Console.WriteLine("{0,-15}{1,-30}{2,-15}{3,-10}{4}", "ProductId", "ProductName", "CategoryId", "Price", "QuantityAvailable");
+                Console.WriteLine("---------------------------------------------------------------------------------------");
+                foreach (var product in lstProd)
+                {
+                    Console.WriteLine("{0,-15}{1,-30}{2,-15}{3,-10}{4}", product.ProductId, product.ProductName, product.CategoryId, product.Price, product.QuantityAvailable);
+                }
+            }
+
+            /*// Add Products
+            Console.WriteLine("\n-----------------------------");
+            Console.WriteLine("Product Details Added");
+            Console.WriteLine("-----------------------------");
+            Product productOne = new Product();
+            productOne.ProductId = "P158";
+            productOne.ProductName = "The Ship of Secrets - Geronimo Stilton";
+            productOne.CategoryId = 8;
+            productOne.Price = 450;
+            productOne.QuantityAvailable = 10;
+
+            Product productTwo = new Product();
+            productTwo.ProductId = "P159";
+            productTwo.ProductName = "101 Nursery Rhymes";
+            productTwo.CategoryId = 8;
+            productTwo.Price = 700;
+            productTwo.QuantityAvailable = 10;
+            bool prodResult = repository.AddProductUsingAddRange(productOne, productTwo);
+            if (prodResult)
+            {
+                Console.WriteLine("Product Details added successfully");
+            }
+            else
+            {
+                Console.WriteLine("Something went wrong. Try again");
+            }*/
+
+            // Update CategoryName by ID
+            Console.WriteLine("\n-----------------------------");
+            Console.WriteLine("Update CategoryName by Id");
+            Console.WriteLine("-----------------------------");
+            bool updateResult = repository.UpdateCategory(8, "Sationary");
+            if(updateResult == true)
+            {
+                Console.WriteLine("CategoryName has been updated");
+            }
+            else
+            {
+                Console.WriteLine("Something went wrong, CategoryName has not been updated");
+            }
+
+            /*// Update Product Price by Id
+            Console.WriteLine("\n-----------------------------");
+            Console.WriteLine("Update Product Price by Id");
+            Console.WriteLine("-----------------------------");
+            int status = repository.UpdateProduct("P159", 666);
+            if(status == 1)
+            {
+                Console.WriteLine("Product Price Updated Successfully");
+            }
+            else
+            {
+                Console.WriteLine("Something went wrong, try again!");
+            }*/
+
+            // Update Multiple Product by Id
+            Console.WriteLine("\n----------------------------------------------------------");
+            Console.WriteLine("Update Multiple Product Quantity with the same CategoryId");
+            Console.WriteLine("----------------------------------------------------------");
+            int prodStatus = repository.UpdateProductUsingUpdateRange(1, 15);
+            if(prodStatus == 1)
+            {
+                Console.WriteLine("Products updated successfully");
+            }
+            else
+            {
+                Console.WriteLine("Something went wrong, try again");
+            }
+
+            /*// Delete Product
+            Console.WriteLine("\n-----------------------------");
+            Console.WriteLine("Delete Product");
+            Console.WriteLine("-----------------------------");
+            bool deleteProd = repository.DeleteProduct("P159");
+            if(deleteProd == true)
+            {
+                Console.WriteLine("Product has been successfully deleted");
+            }
+            else
+            {
+                Console.WriteLine("Something went wrong, product has not been deleted");
+            }*/
+
+            // Delete Product using range
+            Console.WriteLine("\n------------------------------------------");
+            Console.WriteLine("Delete Product Using Range by ProductName");
+            Console.WriteLine("-------------------------------------------");
+            bool delteRange = repository.DeleteProductUsingRange("BMW"); // Delte any product that has BMW in it
+            if (delteRange == true)
+            {
+                Console.WriteLine("Products has been successfully deleted");
+            }
+            else
+            {
+                Console.WriteLine("Something went wrong, product has not been deleted");
+            }
+        }
+    }
+}
---
+++ Infosys.DBFirstCore.DataAccessLayer/QuickKartRepository.cs
@@ -0,0 +1,206 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Infosys.DBFirstCore.DataAccessLayer.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infosys.DBFirstCore.DataAccessLayer
+{
+    public class QuickKartRepository
+    {
+        QuickKartDbContext context;
+        public QuickKartRepository()
+        {
+            context = new QuickKartDbContext();
+        }
+
+        public List<Category> GetAllCategories()
+        {
+            // LINQ query
+            var categoriesList = (from category in context.Categories
+                                  orderby category.CategoryId
+                                  select category).ToList(); 
+            return categoriesList;
+        }
+
+        // Filer product by Id
+        public List<Product> GetProductsOnCategoryId(byte categoryId)
+        {
+            List<Product> products = null;
+
+            try
+            {
+                products = context.Products.Where(p => p.CategoryId == categoryId).ToList();
+            }
+            catch(Exception ex)
+            {
+                products = null;
+            }
+
+            return products;
+        }
+
+        // Filter product by category
+        public Product FilterProducts(byte categoryId)
+        {
+            Product prod = null;
+            try
+            {
+                prod = context.Products.Where(p => p.CategoryId == categoryId).OrderByDescending(p => p.Price).LastOrDefault();
+            }
+            catch (Exception ex)
+            {
+                prod = null;
+            }
+            return prod;
+        }
+
+        // Filter product by pattern
+        public List<Product> FilterProductsUsingLikes(string pattern)
+        {
+            List<Product> lstproduct = null;
+            try
+            {
+                lstproduct = context.Products.Where(p => EF.Functions.Like(p.ProductName, pattern)).ToList();
+            }
+            catch (Exception ex)
+            {
+                lstproduct = null;
+            }
+            return lstproduct;
+        }
+
+        // Add Product using AddRange()
+        public bool AddProductUsingAddRange(params Product[] products)
+        {
+            bool stats = false;
+            try
+            {
+                context.AddRange(products);
+                context.SaveChanges();
+                stats = true;
+            }
+            catch (Exception)
+            {
+                stats = false;
+            }
+            return stats;
+        }
+
+        // Update Category
+        public bool UpdateCategory(byte categoryId, string newCategoryName)
+        {
+            bool status = false;
+            Category category = context.Categories.Find(categoryId);
+
+            try
+            {
+                if (category != null)
+                {
+                    category.CategoryName = newCategoryName;
+                    context.SaveChanges();
+                    status = true;
+                }
+                else
+                {
+                    status = false;
+                }
+            }
+            catch(Exception)
+            {
+                status = false;
+            }
+
+            return status;
+        }
+
+        // Update Product
+        public int UpdateProduct(string productId, decimal price)
+        {
+            int status = -1;
+            try
+            {
+                Product prod = context.Products.Find(productId);
+                prod.Price = price;
+                using (var newContext = new QuickKartDbContext())
+                {
+                    newContext.Products.Update(prod);
+                    newContext.SaveChanges();
+                    status = 1;
+                }
+            }
+            catch (Exception)
+            {
+                status = -99;
+            }
+            return status;
+        }
+
+        // Update Multiple Product by Range
+        public int UpdateProductUsingUpdateRange(byte categoryId, int quantityProcured)
+        {
+            int status = -1;
+            try
+            {
+                List<Product> productList = context.Products.Where(p => p.CategoryId == categoryId).ToList();
+                foreach(var product in productList)
+                {
+                    product.QuantityAvailable = quantityProcured;
+                }
+                using(var newContext = new QuickKartDbContext())
+                {
+                    newContext.UpdateRange(productList);
+                    newContext.SaveChanges();
+                    status = 1;
+                }
+            }
+            catch (Exception)
+            {
+                status = -99;
+            }
+            return status;
+        }
+
+        // Delete Product
+        public bool DeleteProduct(string productId)
+        {
+            bool status = false;
+            try
+            {
+                Product product = context.Products.Find(productId);
+                if(product != null)
+                {
+                    context.Products.Remove(product);
+                    context.SaveChanges();
+                    status = true;
+                }
+                else
+                {
+                    status = false;
+                }
+            }
+            catch (Exception)
+            {
+                status = false;
+            }          
+            return status;
+        }
+
+        // Delete Product using range
+        public bool DeleteProductUsingRange(string subString)
+        {
+            bool status = false;
+            try
+            {
+                var delteProduct = context.Products.Where(p => p.ProductName.Contains(subString)); 
+                context.Products.RemoveRange(delteProduct);
+                context.SaveChanges();
+                status = true;
+            }
+            catch (Exception)
+            {
+                status = false;
+            }
+            return status;
+        }
+    }
+}
